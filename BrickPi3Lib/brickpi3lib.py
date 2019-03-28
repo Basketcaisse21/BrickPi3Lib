c65ef86f8b8d0c5368e80317c2a17fd1f7a075e1
Emil Jimenez <Basketcaisse21@users.noreply.github.com>
2019-03-27 23:55:20 -0400

    Code Cleanup

--- BrickPi3Lib/brickpi3lib.py
+++ BrickPi3Lib/brickpi3lib.py
@@ -1,45 +1,48 @@
 import brickpi3
-import time
 
 class Sensor:
 	bp = None
 	type = None
 	slot = None
+	slots = {
+		"s1": self.bp.PORT_1,
+		"s2": self.bp.PORT_2,
+		"s3": self.bp.PORT_3,
+		"s4": self.bp.PORT_4
+	}
 	
 	def __init__(self, slot, type):
 		self.bp = brickpi3.BrickPi3()
 		slot = self.__setSlot(slot)
 		if slot:
-			self.slot = slot
-			if type == "ultrasonic" or type == "ultra" or type=="us":
-				self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_ULTRASONIC_CM)
-			elif type == "infrared" or type == "ir":
-				self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_INFRARED_REMOTE)
-			elif type == "infraredproximity" or type == "irproxmiity" or type == "irp":
-				self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_INFRARED_PROXIMITY)
-			elif type == "gyro":
-				self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_GYRO_ABS_DPS)
-			elif type == "color" or type == "raw":
-				self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_COLOR_COLOR)
-			else:
-				return False
-			
-		else:
-			return False
+			self.__setType(type)
 			
 	def __setSlot(self, slot):
 		slot = slot.lower()
-		if slot == "s1":
-			return self.bp.PORT_1
-		if slot == "s2":
-			return self.bp.PORT_2
-		if slot == "s3":
-			return self.bp.PORT_3
-		if slot == "s4":
-			return self.bp.PORT_4
-		return None
+		if slot in self.slots:
+			selt.slot = slot
+			return True
+		return False
+	
+	def __setType(self, type):
+		if type in ["ultrasonic", "ultra", "us"]:
+			self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_ULTRASONIC_CM)
+			return True
+		elif type in ["infrared", "ir"]:
+			self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_INFRARED_REMOTE)
+			return True
+		elif type in ["infraredproximity", "irproxmiity", "irp"]:
+			self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_INFRARED_PROXIMITY)
+			return True
+		elif type in ["gyro"]:
+			self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_GYRO_ABS_DPS)
+			return True
+		elif type in ["color", "raw"]:
+			self.bp.set_sensor_type(slot, self.bp.SENSOR_TYPE.EV3_COLOR_COLOR)
+			return True
+		return False
 		
-	def getVal(self):
+	def getReading(self):
 		return self.bp.get_sensor(self.slot)
 		
 	def reset(self):
@@ -49,28 +52,32 @@ class Motor:
 	bp = None
 	type = None
 	slot = None
+	slots = {
+		'ma': self.bp.PORT_A,
+		'mb': self.bp.PORT_B,
+		'mc': self.bp.PORT_C,
+		'md': self.bp.PORT_D
+	}
 	
 	def __init__(self, slot):
 		self.bp = brickpi3.BrickPi3()
 		slot = self.__setSlot(slot)
-		if slot:
-			self.slot = slot
-			self.setSpeed(0)
 	
 	def __setSlot(self, slot):
 		slot = slot.lower()
-		if slot == "ma":
-			return self.bp.PORT_A
-		if slot == "mb":
-			return self.bp.PORT_B
-		if slot == "mc":
-			return self.bp.PORT_C
-		if slot == "md":
-			return self.bp.PORT_D
-		return None
+		if slot in self.slots:
+			self.slot = slots[slot]
+			self.setSpeed(0)
+			return True
+		return False
 			
 	def setSpeed(self, speed):
-		self.bp.set_motor_power(self.slot, speed)
-
-
-
+		try:
+			self.bp.set_motor_power(self.slot, speed)
+			return True
+		except:
+			return False
+		
+	def reset(self):
+		return self.bp.reset_all()
+			
